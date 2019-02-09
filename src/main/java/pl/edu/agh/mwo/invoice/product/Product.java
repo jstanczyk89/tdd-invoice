7f0053002cc4dc79addd77644cf34a980f892882
student12 <student12@S7N-PROGLANG-1.pcoip.ki.agh.edu.pl>
2019-02-09 13:55:51 +0100

    First tests are working

--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -9,25 +9,31 @@ public abstract class Product {
 
 	private final BigDecimal taxPercent;
 
-	protected Product(String name, BigDecimal price, BigDecimal tax) {
+	protected Product(String name, BigDecimal price, BigDecimal tax) throws IllegalArgumentException{
+		if (name.equals(null)){
+			throw new IllegalArgumentException("Towar musi miec nazwe");
+		}
 		this.name = name;
 		this.price = price;
 		this.taxPercent = tax;
 	}
 
-	public String getName() {
-		return null;
+	public String getName() throws IllegalArgumentException {
+	
+		return this.name;
 	}
 
 	public BigDecimal getPrice() {
-		return null;
+		return this.price;
 	}
 
 	public BigDecimal getTaxPercent() {
-		return null;
+		return this.taxPercent;
 	}
 
 	public BigDecimal getPriceWithTax() {
-		return null;
+		
+		
+		return (this.price.multiply(this.taxPercent)).add(price) ;
 	}
 }
